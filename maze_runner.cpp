d459e344a6e51d3a5cc941821ff41267e842a933
renanzao <renannevessilva@gmail.com>
2024-10-06 16:50:13 -0300

    quase finalizando, de verdade agora

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -20,7 +20,6 @@ int num_rows;
 int num_cols;
 std::stack<Position> valid_positions;
 
-// Função para carregar o labirinto de um arquivo
 Position load_maze(const std::string& file_name) {
     std::ifstream arquivo(file_name);
     if (!arquivo.is_open()) {
@@ -28,8 +27,9 @@ Position load_maze(const std::string& file_name) {
         return {-1, -1};
     }
 
-    // Lendo o número de linhas e colunas
-    arquivo >> num_rows >> num_cols;
+   
+    
+      arquivo >> num_rows >> num_cols;
     maze.resize(num_rows, std::vector<char>(num_cols));
 
     Position start{-1, -1};
@@ -37,45 +37,41 @@ Position load_maze(const std::string& file_name) {
         for (int col = 0; col < num_cols; ++col) {
             arquivo >> maze[row][col];
             if (maze[row][col] == 'e') {
-                start = {row, col}; // Encontrando a posição inicial
+                start = {row, col}; 
             }
         }
     }
 
     arquivo.close();
-    return start; // Retornando a posição inicial
+    return start; 
 }
 
-// Função para imprimir o labirinto
+
 void print_maze() {
     for (const auto& row : maze) {
         for (char cell : row) {
             std::cout << cell;
         }
-        std::cout << '\n'; // Adicionando quebra de linha ao final de cada linha
+        std::cout << '\n'; 
     }
 }
 
-// Função para verificar se uma posição é válida
 bool is_valid_position(int row, int col) {
     return (row >= 0 && row < num_rows && col >= 0 && col < num_cols && maze[row][col] == 'x');
 }
 
 bool walk(Position pos) {
-    // Marcar a posição atual como visitada
     maze[pos.row][pos.col] = '.';
     print_maze();
 
-    // Adicionando um atraso de meio segundo (500 milissegundos)
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    // Verificar se a posição atual é a saída
     if (maze[pos.row][pos.col] == 's') {
         return true;
     }
 
-    // Verificando posições adjacentes (cima, baixo, esquerda, direita)
-    Position directions[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Cima, Baixo, Esquerda, Direita
+   
+    Position directions[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
     for (const auto& dir : directions) {
         int new_row = pos.row + dir.row;
         int new_col = pos.col + dir.col;
@@ -85,18 +81,18 @@ bool walk(Position pos) {
         }
     }
 
-    // Enquanto houver posições válidas na pilha
+    
     while (!valid_positions.empty()) {
         Position next_pos = valid_positions.top();
         valid_positions.pop();
 
-        // Chame walk recursivamente para esta posição
+       
         if (walk(next_pos)) {
-            return true; // Se encontrar a saída, retorne true
+            return true; 
         }
     }
 
-    return false; // Se todas as posições foram exploradas sem encontrar a saída
+    return false;
 }
 
 
